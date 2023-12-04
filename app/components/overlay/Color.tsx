1d430cfd4f82f20845d140cc3385662b254e8769
Llewellyn Paintsil <llewellynpaintsil34@gmail.com>
2023-12-04 01:02:22 +0000

    updated logo

--- app/components/overlay/Color.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Color: React.FC = () => {
-    return (
-        <div className="fixed inset-0 p-1 w-full h-full bg-gradient-to-t from-bg-dark to-transparent opacity-40 pointer-events-none z-40"/>
-    );
-};
-
-export default Color;
