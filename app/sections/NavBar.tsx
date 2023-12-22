7c764427b71d50290418107692595a0d242f2eeb
Llewellyn Paintsil <llewellynpaintsil34@gmail.com>
2023-12-22 16:40:28 +0000

    increased width of navbar

--- app/sections/NavBar.tsx
+++ app/sections/NavBar.tsx
@@ -17,7 +17,7 @@ const NavBar = () => {
     return (
         <nav className="nowrap fixed bottom-10 left-0 right-0 z-50 my-0 mx-auto flex items-center justify-center gap-1 px-1 py-1 text-[#2196f3] sm:w-[383.3px] md:p-2 lg:w-[391.3px]">
             <NavContainer
-                width="100%"
+                width="110%"
                 height="50px"
                 color="#141413"
                 borderRadius={10}
